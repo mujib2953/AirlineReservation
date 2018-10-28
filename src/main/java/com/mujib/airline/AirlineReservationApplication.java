193f9708680e21c2e53e93958b215eb5a9db8237
Mujib Ansari <ansarimujiburrehman@gmail.com>
2018-10-28 11:08:45 +0530

    Project started

---
+++ src/main/java/com/mujib/airline/AirlineReservationApplication.java
@@ -0,0 +1,12 @@
+package com.mujib.airline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirlineReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirlineReservationApplication.class, args);
+	}
+}
